c4ad2e8ddb82ee015d54db2ea87641260aa073d2
yassinembh <yassinembh@gmail.com>
2022-10-28 01:02:00 +0100

    aaafff

---
+++ src/main/java/haya/yassine/App.java
@@ -0,0 +1,13 @@
+package haya.yassine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
